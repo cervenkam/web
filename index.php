dc6c8ad80494fde95d59bde13f5c32a7720c33ca
maskacovnik <maskacovik@seznam.cz>
2016-09-23 18:02:07 +0200

    First template

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 	require_once 'twig/lib/Twig/Autoloader.php';
 	Twig_Autoloader::register();
-	$loader = new Twig_Loader_Array(array(
-		'index' => 'Hello {{ name }}!'
-	));
+	$loader = new Twig_Loader_Filesystem('templates');
 	$twig = new Twig_Environment($loader);
-	echo $twig->render('index',array('name' => 'Fabien'));
+	$template = $twig->loadTemplate('index.tmpl');
+	echo $template->render(array(
+		'name' => 'Fabien',
+		'title' => 'Title'
+	));
 ?>
